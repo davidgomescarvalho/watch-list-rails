6f9e2f789bce011baff19d2578926cbd530b10ac
david gomes <davidgomesk8@gmail.com>
2024-07-23 19:17:55 +0200

    validations and associations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,7 @@
 class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :movie_id, uniqueness: { scope: :list_id, message: "is already in the list" }
+  validates :comment, length: { minimun: 6}
 end
