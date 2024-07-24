a388427338bed45577121287419aadbe7c2ff195
david gomes <davidgomesk8@gmail.com>
2024-07-24 18:45:21 +0200

    controllers done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,5 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :movie_id, uniqueness: { scope: :list_id, message: "is already in the list" }
-  validates :comment, length: { minimun: 6}
+  validates :comment, length: { minimum: 6}
 end
